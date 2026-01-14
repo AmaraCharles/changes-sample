d59a722a525fef1d532c7748ca174ddba089c46b
AmaraCharles <falsepegasus@gmail.com>
2026-01-14 10:07:07 +0100

    tags

--- server/email.ts
+++ server/email.ts
@@ -5,7 +5,7 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY || "");
 const FROM_EMAIL =
-  process.env.FROM_EMAIL || "Resend <onboarding@resend.dev>";
+  process.env.FROM_EMAIL || "Ethergalleries <support@Ethergalleries.com>";
 
 // ----------------------------
 // Generic send helper
