d466cc7eedb9f398d06fee0dfbfb8dc2dd266bb8
AmaraCharles <falsepegasus@gmail.com>
2026-02-17 11:01:04 +0100

    aadmlm

--- server/index.ts
+++ server/index.ts
@@ -23,6 +23,7 @@ const allowedOrigins = [
   "https://blocktoart.com",
   "https://www.blocktoart.com",
   "http://localhost:5000",
+  "http://localhost:5001",
   "http://127.0.0.1:5503",
   "http://127.0.0.1:5504"
 ];
